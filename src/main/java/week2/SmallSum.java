962c0ecc614876e398deed3a9a04c4b35220ab55
xingzhou <1963806558@qq.com>
2022-01-12 23:11:39 +0800

    smallSum

---
+++ src/main/java/week2/SmallSum.java
@@ -0,0 +1,48 @@
+package week2;
+
+/**
+ * @author xingzhou
+ */
+public class SmallSum {
+    public static int smallSum(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return 0;
+        }
+        return process(arr, 0, arr.length - 1);
+    }
+
+    private static int process(int[] arr, int left, int right) {
+        if (left == right) {
+            return 0;
+        }
+        int mid = left + ((right - left) >> 1);
+        return process(arr, left, mid) + process(arr, mid + 1, right) + merge(arr, left, mid, right);
+    }
+
+    private static int merge(int[] arr, int left, int mid, int right) {
+        int[] help = new int[right - left + 1];
+        int index = 0;
+        int p1 = left;
+        int p2 = mid + 1;
+        int res = 0;
+        while (p1 <= mid && p2 <= right) {
+            res += arr[p1] < arr[p2] ? (right - p2 + 1) * arr[p1] : 0;
+            help[index++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
+        }
+        while (p1 <= mid) {
+            help[index++] = arr[p1++];
+        }
+        while (p2 <= mid) {
+            help[index++] = arr[p2++];
+        }
+        for (int i = 0; i < help.length; i++) {
+            arr[left + i] = help[i];
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 1, 3};
+        System.out.println(smallSum(arr));
+    }
+}

39e1640ba72c10fad9a3cf0892dfb2b090fe207e
xingzhou <1963806558@qq.com>
2022-01-30 22:52:45 +0800

    FindFirstIntersectNode

--- src/main/java/week4/FindFirstIntersectNode.java
+++ src/main/java/week4/FindFirstIntersectNode.java
@@ -128,47 +128,4 @@ public class FindFirstIntersectNode {
             return null;
         }
     }
-    public static void main(String[] args) {
-        // 1->2->3->4->5->6->7->null
-        Node head1 = new Node(1);
-        head1.next = new Node(2);
-        head1.next.next = new Node(3);
-        head1.next.next.next = new Node(4);
-        head1.next.next.next.next = new Node(5);
-        head1.next.next.next.next.next = new Node(6);
-        head1.next.next.next.next.next.next = new Node(7);
-
-        // 0->9->8->6->7->null
-        Node head2 = new Node(0);
-        head2.next = new Node(9);
-        head2.next.next = new Node(8);
-        head2.next.next.next = head1.next.next.next.next.next; // 8->6
-        System.out.println(getIntersectNode(head1, head2).value);
-
-        // 1->2->3->4->5->6->7->4...
-        head1 = new Node(1);
-        head1.next = new Node(2);
-        head1.next.next = new Node(3);
-        head1.next.next.next = new Node(4);
-        head1.next.next.next.next = new Node(5);
-        head1.next.next.next.next.next = new Node(6);
-        head1.next.next.next.next.next.next = new Node(7);
-        head1.next.next.next.next.next.next = head1.next.next.next; // 7->4
-
-        // 0->9->8->2...
-        head2 = new Node(0);
-        head2.next = new Node(9);
-        head2.next.next = new Node(8);
-        head2.next.next.next = head1.next; // 8->2
-        System.out.println(getIntersectNode(head1, head2).value);
-
-        // 0->9->8->6->4->5->6..
-        head2 = new Node(0);
-        head2.next = new Node(9);
-        head2.next.next = new Node(8);
-        head2.next.next.next = head1.next.next.next.next.next; // 8->6
-        System.out.println(getIntersectNode(head1, head2).value);
-
-    }
-
 }

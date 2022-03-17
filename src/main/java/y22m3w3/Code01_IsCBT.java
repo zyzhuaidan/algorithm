70e873678c7f9b64bb477e7eb850ea1c2d0a9b5f
zhuyizheng <zhuyizheng@xiaohongshu.com>
2022-03-17 14:36:25 +0800

    Code01_IsCBT

--- src/main/java/y22m3w3/Code01_IsCBT.java
+++ src/main/java/y22m3w3/Code01_IsCBT.java
@@ -4,31 +4,37 @@ package y22m3w3;
  * @author xingzhou
  * @version 1.0.0
  * @ClassName Code01_IsCBT.java
- * @Description TODO
+ * @Description 判断是否是完全二叉树
  * @createTime 2022年03月16日 11:33:00
  */
 public class Code01_IsCBT {
-    class Node{
+    class Node {
         int value;
         Node left;
         Node right;
-        Node(int value){
+
+        Node(int value) {
             this.value = value;
         }
     }
 
-    class Info{
+    class Info {
+        //是否是满二叉树
         public boolean isFull;
+        //是否是 完全二叉树
         public boolean isCBT;
+        //二叉树的高度
         public int height;
-        public Info(boolean isFull, boolean isCBT, int height){
+
+        public Info(boolean isFull, boolean isCBT, int height) {
             this.isFull = isFull;
             this.isCBT = isCBT;
             this.height = height;
         }
     }
-    Info process(Node x){
-        if(x == null){
+
+    Info process(Node x) {
+        if (x == null) {
             return new Info(true, true, 0);
         }
         Info leftInfo = process(x.left);
@@ -36,22 +42,22 @@ public class Code01_IsCBT {
         int height = Math.max(leftInfo.height, rightInfo.height) + 1;
         boolean isFull = leftInfo.isFull && rightInfo.isFull && rightInfo.height == leftInfo.height;
         boolean isCBT = false;
-        if(isFull){
+        if (isFull) {
             isCBT = true;
-        }else {
-            if(leftInfo.isCBT && rightInfo.isCBT){
-                if(leftInfo.isCBT && rightInfo.isFull && leftInfo.height == rightInfo.height + 1){
+        } else {
+            if (leftInfo.isCBT && rightInfo.isCBT) {
+                if (leftInfo.isCBT && rightInfo.isFull && leftInfo.height == rightInfo.height + 1) {
                     isCBT = true;
                 }
-                if(leftInfo.isFull && rightInfo.isFull && leftInfo.height == rightInfo.height + 1){
+                if (leftInfo.isFull && rightInfo.isFull && leftInfo.height == rightInfo.height + 1) {
                     isCBT = true;
                 }
-                if(leftInfo.isFull && rightInfo.isCBT && leftInfo.height == rightInfo.height){
+                if (leftInfo.isFull && rightInfo.isCBT && leftInfo.height == rightInfo.height) {
                     isCBT = true;
                 }
             }
         }
-        return new Info(isFull,isCBT, height);
+        return new Info(isFull, isCBT, height);
     }
 
 }

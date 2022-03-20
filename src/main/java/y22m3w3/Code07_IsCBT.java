f94cf3e16bbc8f4ce86e7c2b09edd3aa590bf65a
zyzhuaidan <1963806558@qq.com>
2022-03-20 14:38:15 +0800

    Code01_lowestAncestor

---
+++ src/main/java/y22m3w3/Code07_IsCBT.java
@@ -0,0 +1,13 @@
+package y22m3w3;
+
+/**
+ * @author xingzhou
+ * @version 1.0.0
+ * @ClassName Code07_IsCBT.java
+ * @Description TODO
+ * @createTime 2022年03月18日 17:11:00
+ */
+public class Code07_IsCBT {
+
+
+}
---
+++ src/main/java/y22m3w4/Code01_lowestAncestor.java
@@ -0,0 +1,53 @@
+package y22m3w4;
+
+/**
+ * @author xingzhou
+ * @version 1.0.0
+ * @ClassName Code01_lowestAncestor.java
+ * @Description TODO
+ * @createTime 2022年03月20日 14:27:00
+ */
+public class Code01_lowestAncestor {
+    class Node {
+        Node left;
+        Node right;
+        int value;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    class Info {
+        boolean isFindA;
+        boolean isFindB;
+        Node ans;
+
+        public Info(boolean isFindA, boolean isFindB, Node ans) {
+            this.isFindA = isFindA;
+            this.isFindB = isFindB;
+            this.ans = ans;
+        }
+    }
+
+    public Info process(Node x, Node a, Node b) {
+        if (x == null) {
+            return new Info(false, false, null);
+        }
+        Info leftInfo = process(x.left, a, b);
+        Info rightInfo = process(x.right, a, b);
+        boolean isFindA = (x == a) || leftInfo.isFindA || rightInfo.isFindA;
+        boolean isFindB = (x == b) || leftInfo.isFindB || rightInfo.isFindB;
+        Node ans = null;
+        if (leftInfo.ans != null) {
+            ans = leftInfo.ans;
+        } else if (rightInfo.ans != null) {
+            ans = rightInfo.ans;
+        } else {
+            if (isFindA && isFindB) {
+                ans = x;
+            }
+        }
+        return new Info(isFindA, isFindB, ans);
+    }
+}

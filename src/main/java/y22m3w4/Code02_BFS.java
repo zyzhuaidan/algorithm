8ad4bf6a4bfcdf1638f59a97a48965d4ef6c512c
zyzhuaidan <1963806558@qq.com>
2022-03-23 21:56:21 +0800

    Code01_lowestAncestor

---
+++ src/main/java/y22m3w4/Code02_BFS.java
@@ -0,0 +1,32 @@
+package y22m3w4;
+
+import java.util.*;
+
+/**
+ * @author xingzhou
+ * @version 1.0.0
+ * @ClassName Code02_BFS.java
+ * @Description TODO
+ * @createTime 2022年03月21日 10:59:00
+ */
+public class Code02_BFS {
+    public void graphBFS(Node start) {
+        if (start == null) {
+            return;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        HashSet<Node> set = new HashSet<>();
+        queue.add(start);
+        set.add(start);
+        while (!queue.isEmpty()) {
+            Node cur = queue.poll();
+            System.out.println(cur.value);
+            for (Node next : cur.nexts) {
+                if (!set.contains(next)) {
+                    set.add(next);
+                    queue.add(next);
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/y22m3w4/Node.java
@@ -0,0 +1,26 @@
+package y22m3w4;
+
+import java.util.ArrayList;
+
+/**
+ * @author xingzhou
+ * @version 1.0.0
+ * @ClassName Node.java
+ * @Description TODO
+ * @createTime 2022年03月21日 10:38:00
+ */
+public class Node {
+    int out;
+    int in;
+    int value;
+    ArrayList<Node>  nexts;
+    ArrayList<Edge> edges;
+    public Node(int value){
+        this.out = 0;
+        this.in = 0;
+        this.value = value;
+        this.nexts = new ArrayList<>();
+        this.edges = new ArrayList<>();
+    }
+
+}

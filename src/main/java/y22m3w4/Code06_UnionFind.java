e72d39afe6abacd18d4f9face1902334a3effe6a
zyzhuaidan <1963806558@qq.com>
2022-03-27 22:54:43 +0800

    Code06_UnionFind

--- src/main/java/y22m3w4/Code06_UnionFind.java
+++ src/main/java/y22m3w4/Code06_UnionFind.java
@@ -1,6 +1,7 @@
 package y22m3w4;
 
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * @author xingzhou
@@ -20,6 +21,19 @@ public class Code06_UnionFind {
         public HashMap<V, Node<V>> nodes;
         public HashMap<Node<V>, Node<V>> parents;
         public HashMap<Node<V>, Integer> sizeMap;
+        public UnionFind(List<V> values) {
+            nodes = new HashMap<>();
+            parents = new HashMap<>();
+            sizeMap = new HashMap<>();
+            for(V cur:values){
+                Node<V> node = new Node<>(cur);
+                nodes.put(cur, node);
+                parents.put(node,node);
+                sizeMap.put(node, 1);
+            }
+
+
+        }
 
     }
 }

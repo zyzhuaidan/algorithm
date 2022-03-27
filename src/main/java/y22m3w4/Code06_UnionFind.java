d5941261a33bbe9d2003e007f94494239f10d021
zyzhuaidan <1963806558@qq.com>
2022-03-27 23:10:34 +0800

    Code06_UnionFind

--- src/main/java/y22m3w4/Code06_UnionFind.java
+++ src/main/java/y22m3w4/Code06_UnionFind.java
@@ -2,6 +2,7 @@ package y22m3w4;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * @author xingzhou
@@ -31,9 +32,37 @@ public class Code06_UnionFind {
                 parents.put(node,node);
                 sizeMap.put(node, 1);
             }
-
-
         }
-
+        // 给你一个节点 网上到不能再往上，把代表返回
+        public Node<V> findFather(Node<V> cur){
+            Stack<Node<V>> path = new Stack<>();
+            while (cur != parents.get(cur)){
+                path.push(cur);
+                cur = parents.get(cur);
+            }
+            while(!path.isEmpty()){
+                parents.put(path.pop(), cur);
+            }
+            return cur;
+        }
+        public boolean isSameSet(V a, V b){
+            return findFather(nodes.get(a)) == findFather(nodes.get(b));
+        }
+        public void union(V a, V b){
+            Node<V> aHead = findFather(nodes.get(a));
+            Node<V> bHead = findFather(nodes.get(b));
+            if(aHead != bHead){
+                int aSetSize = sizeMap.get(aHead);
+                int bSetSize = sizeMap.get(bHead);
+                Node<V> big = aSetSize >= bSetSize ? aHead : bHead;
+                Node<V> small = big == aHead ? bHead : aHead;
+                parents.put(small, big);
+                sizeMap.put(big, aSetSize + bSetSize);
+                sizeMap.remove(small);
+            }
+        }
+        public int sets(){
+            return sizeMap.size();
+        }
     }
 }
